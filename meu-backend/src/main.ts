a14399c79155d13087a287db0906d6e593b65a6b
vutiendat24 <vutiendattech@gmail.com>
2026-07-06 12:31:56 +0700

    fix: them .env

--- meu-backend/src/main.ts
+++ meu-backend/src/main.ts
@@ -5,23 +5,6 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Enable Global Validation
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
-
-  // Enable CORS
-  app.enableCors();
-
-  const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') || 8000;
-
-  await app.listen(port);
-  console.log(`Backend Application is running on: http://localhost:${port}`);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
